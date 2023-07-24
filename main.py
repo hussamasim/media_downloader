a91e3e72a38a93962c2c17dbe035d4c57fdc50ca
Hussam <mhussam.asim@gmail.com>
2023-07-24 00:07:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+import re
+import tkinter as tk
+from tkinter import ttk
+import yt_dlp
+
+def get_input():
+    url = urlTextBox.get()
+    if formatVar.get() == 1:
+        download_mp3(url)
+    elif formatVar.get() == 0:
+        download_mp4(url)
+
+def download_mp3(url):
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "mp3"}],
+        "progress_hooks": [progress_hook],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydlp:
+        ydlp.download([url])
+
+def download_mp4(url):
+    ydl_opts = {
+        "format": "best",
+        "progress_hooks": [progress_hook],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydlp:
+        ydlp.download([url])
+
+def remove_escape_sequences(text):
+    # Use regex to remove escape sequences (color codes) from the text
+    return re.sub(r'\x1b\[[0-9;]*m', '', text)
+
+def progress_hook(d):
+    if d['status'] == 'downloading':
+        progress_str = remove_escape_sequences(d['_percent_str'])
+        progress = re.search(r'\b\d+(\.\d+)?\b', progress_str)
+        if progress:
+            percent = float(progress.group())
+            progress_var.set(percent)
+
+# GUI Elements:
+root = tk.Tk()
+root.title("Media Site Downloader")
+urlLabel = tk.Label(root, text="YouTube URL:")
+urlTextBox = tk.Entry(root, width=100)
+
+formatVar = tk.IntVar()
+
+mp3Check = tk.Radiobutton(root, text="MP3", variable=formatVar, value=1)
+mp4Check = tk.Radiobutton(root, text="MP4", variable=formatVar, value=0)
+
+downloadButton = tk.Button(root, text="Download", width=10, command=get_input)
+
+# Progress Bar
+progress_var = tk.DoubleVar()
+progress_bar = ttk.Progressbar(root, variable=progress_var, length=200)
+
+
+# Button to open file location browse
+browseButton = tk.Button(root, text="Browse", width=10, command=browse_destination)
+browseButton.grid(row=2, column=3)
+
+destination_var = tk.StringVar()
+destination_entry = tk.Entry(root, width=100, textvariable=destination_var, state="readonly")
+destination_entry.grid(row=2, column=1, columnspan=2)
+
+# GUI Formatting:
+root.geometry("800x480+400+240")
+urlLabel.grid(row=0, column=0)
+urlTextBox.grid(row=0, column=1)
+mp3Check.grid(row=1, column=0)
+mp4Check.grid(row=1, column=1)
+downloadButton.grid(row=0, column=2)
+progress_bar.grid(row=2, columnspan=3, pady=10)
+
+root.mainloop()
+
+
+# update UI
+# file location save as
+# error handling
+# dynamic progress bar
+# embed metadata option
---
+++ package_app.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+import os
+
+# Replace 'your_script.py' with the name of your existing script file
+script_file = "./main.py"
+output_folder = "dist"
+
+# Ensure that the output folder exists
+os.makedirs(output_folder, exist_ok=True)
+
+# Package the script using PyInstaller
+cmd = f"pyinstaller --onefile --windowed --distpath={output_folder} {script_file}"
+subprocess.run(cmd, shell=True)
+
+# Print a success message
+print("Packaging completed!")
