0a65c370b723fb919e64c3c3afab06bfb72df25c
Hussam <mhussam.asim@gmail.com>
2023-07-24 00:17:15 -0500

    Added file dest functionality

--- package_app.py
+++ package_app.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Replace 'your_script.py' with the name of your existing script file
-script_file = "./main.py"
+script_file = "media_downloader.py"
 output_folder = "dist"
 
 # Ensure that the output folder exists
